a908c6577780a2b8f736818d9f5be39679dd9a57
Your Name <you@example.com>
2025-04-03 17:30:29 +0500

    10

--- config/settings.py
+++ config/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = 'django-insecure-sfxtedi$dh=#e!n=#nwmi35^(26o0(z556j5-7d+^%e#n3t6$z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['farhodmardex.uzfati.uz', '127.0.0.1']
+ALLOWED_HOSTS = ['farhodmardex.uzfati.uz', 'localhost', '127.0.0.1']
 
 
 # Application definition
